877dc3f7ef2dd46943cac206cf11cfa7d53df5d9
Aaron Donelson <aaron.donelson23@gmail.com>
2021-10-07 21:12:38 -0400

    added function to read the json file, post created for api/notes, app.delete created for removal of notes

--- Develop/server.js
+++ Develop/server.js
@@ -23,36 +23,59 @@ app.get('/api/notes', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+const contentWrite = (contentDestination, content) => 
+fs.writeFile(contentDestination, JSON.stringify(content, null, 4), (err) =>
+err ? console.error(err) : console.info(`\nData written to ${contentDestination}`)
+);
 
-app.post("/api/notes", (req, res) => {
-    console.log(`${req.method} request received new note added`);
-    
-    const { title, text, id } = req.body;
+// function  which will read data from json the file
+const appendFile = (content, file) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+        }
+    });
+};
 
-    if (title && text) {
-        const note = {
+// post route to allow new submissions which will append them to the json file.
+app.post('/api/notes', (req, res) => {
+    const { title, text } = req.body;
+    if (req.body) {
+        const newUserNote = {
             title,
             text,
-            review_id: uuidv(),
+            id: uuid(),
         };
-
-        const noteString = JSON.stringify(note);
-
-        fs.appendFile("./db/db.json", noteString, (err) =>
-            err ? console.error(err) : console.log(`Note ${note.title} has been written to JSON file`)
-        );
-
-        const response = {
-            status: "Submitted",
-            body: note,
-        };
-
-        console.log(response);
-        res.status(201).json(response);
+        AppendFile(newUserNote, './db/db.json');
+        takenNotesArray.push(newUserNote)
+        console.log(takenNotesArray);
+        res.json(`Your note has been added`);
     } else {
-        res.status(400).json('Your note had an error in posting the note');
+        res.error('We had an issue adding your note');
     }
+});
+
+
+// This route should allow users to delete previously added notes.
+app.delete(`/api/notes/:id`, (req, res) => {
+    for (let i = 0; i < notesArray.length; i++) {
+        if (notesArray[i].id === req.params.id) {
+            notesArray.splice(i, 1)
+            break;
+        }
+    }
+    contentWrite("./db/db.json", notesFile); // file will now be changed to support the deleted file. 
+    res.json(notesFile)
 })
+// get request that will return html file 
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+// listens for server port location. Will log the port location in the console. 
 
 app.listen(PORT, () =>
     console.log(`You are listening on port ${PORT}!`)
